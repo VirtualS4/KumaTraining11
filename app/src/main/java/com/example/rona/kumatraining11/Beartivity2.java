6d9698a1e4d63b98bdcf2113a97d87f9c1d2bf61
VirtualS4 <Oponggunarko@yahoo.co.id>
2019-09-02 09:39:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rona/kumatraining11/Beartivity2.java
@@ -0,0 +1,118 @@
+package com.example.rona.kumatraining11;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.os.Handler;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Beartivity2 extends AppCompatActivity {
+    TextView textView;
+    Button start, pause, reset, lap;
+
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds;
+    ListView listView;
+    String[] ListElements = new String[]{};
+    List<String> ListElementsArrayList;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beartivity2);
+
+        textView = (TextView) findViewById(R.id.textView);
+        start = (Button) findViewById(R.id.btn_mulai);
+        pause = (Button) findViewById(R.id.btn_berhenti);
+        reset = (Button) findViewById(R.id.btn_reset);
+        lap = (Button) findViewById(R.id.btn_split);
+        listView = (ListView) findViewById(R.id.listview1);
+
+        handler = new Handler();
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(Beartivity2.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+        listView.setAdapter(adapter);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimeBuff += MillisecondTime;
+                handler.removeCallbacks(runnable);
+                reset.setEnabled(true);
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L;
+                StartTime = 0L;
+                TimeBuff = 0L;
+                UpdateTime = 0L;
+                Seconds = 0;
+                Minutes = 0;
+                MilliSeconds = 0;
+                textView.setText("00:00:00");
+                ListElementsArrayList.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListElementsArrayList.add(textView.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+            UpdateTime = TimeBuff + MillisecondTime;
+            Seconds = (int) (UpdateTime / 1000);
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
+
+}
+
